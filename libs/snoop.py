3af11a8d6952e1916e4731d54795bb94c6ac631d
Fabio Viola <desmovalvo@gmail.com>
2016-06-30 11:53:44 +0200

    Added a few helpers

---
+++ libs/snoop.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+# system wide requirements
+from smart_m3.m3_kp_api import *
+
+# local requirements
+from otm_bs_queries import *
+
+# check user uid
+def user_uid_available(settings, user_uid):
+
+    """This method is used to verify if a user uid
+    is free or already in use. It returns False if 
+    the user uid is free, True otherwise"""
+
+    # connect to the sib
+    kp = m3_kp_api(False, self.settings["sib_host"], self.settings["sib_port"])
+    
+    # perform the query
+    kp.load_query_sparql(user_exists)
+    result = str(kp.result_sparql_query[0][0])
+
+    # return True or False
+    if result == "true":
+        return False
+    else:
+        return True
